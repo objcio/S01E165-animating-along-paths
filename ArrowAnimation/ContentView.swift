58265dfb89e9fc82ad174235056be4db49710a4d
Chris Eidhof <chris@eidhof.nl>
2019-08-13 12:58:50 +0200

    Episode 166

--- ArrowAnimation/ContentView.swift
+++ ArrowAnimation/ContentView.swift
@@ -29,7 +29,57 @@ extension Path {
         guard position > 0 else { return cgPath.currentPoint }
         return trimmedPath(from: 0, to: position).cgPath.currentPoint
     }
+
+    func pointAndAngle(at position: CGFloat) -> (CGPoint, Angle) {
+        let p1 = point(at: position)
+        let p2 = point(at: (position + 0.01).truncatingRemainder(dividingBy: 1))
+        let angle = Angle(radians: Double(atan2(p2.y-p1.y, p2.x-p1.x)))
+        return (p1, angle)
+    }
 }
+
+let strokeStyle = StrokeStyle(lineWidth: 3)
+
+struct FollowPath<P: Shape>: GeometryEffect {
+    let pathShape: P
+    var offset: CGFloat // 0...1
+    
+    var animatableData: CGFloat {
+        get { offset }
+        set { offset = newValue }
+    }
+    
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        let path = pathShape.path(in: CGRect(origin: .zero, size: size))
+        let (point, angle) = path.pointAndAngle(at: offset)
+        let affineTransform = CGAffineTransform(translationX: point.x, y: point.y).rotated(by: CGFloat(angle.radians) + .pi/2)
+        return ProjectionTransform(affineTransform)
+    }
+}
+
+struct Trail<P: Shape>: Shape {
+    let pathShape: P
+    var offset: CGFloat // 0...1
+    
+    var animatableData: CGFloat {
+        get { offset }
+        set { offset = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        let path = pathShape.path(in: rect)
+        var result = Path()
+        let trailLength: CGFloat = 0.2
+        let trimFrom = offset - trailLength
+        if trimFrom < 0 {
+            result.addPath(path.trimmedPath(from: trimFrom + 1, to: 1))
+        }
+        result.addPath(path.trimmedPath(from: max(0, trimFrom), to: offset))
+        result = result.strokedPath(strokeStyle)
+        return result
+    }
+}
+
 
 struct OnPath<P: Shape, S: Shape>: Shape {
     var shape: S
@@ -48,19 +98,33 @@ struct OnPath<P: Shape, S: Shape>: Shape {
     
     func path(in rect: CGRect) -> Path {
         let path = pathShape.path(in: rect)
-        let point = path.point(at: offset)
+        let (point, angle) = path.pointAndAngle(at: offset)
         let shapePath = shape.path(in: rect)
         let size = shapePath.boundingRect.size
-        let head = shapePath.offsetBy(dx: point.x - size.width/2, dy: point.y - size.height/2)
+        let head = shapePath
+            .offsetBy(dx: -size.width/2, dy: -size.height/2)
+            .applying(CGAffineTransform(rotationAngle: CGFloat(angle.radians) + .pi/2))
+            .offsetBy(dx: point.x, dy: point.y)
         var result = Path()
         let trailLength: CGFloat = 0.2
         let trimFrom = offset - trailLength
         if trimFrom < 0 {
-            result.addPath(path.trimmedPath(from: trimFrom + 1, to: 1).strokedPath(.init()))
+            result.addPath(path.trimmedPath(from: trimFrom + 1, to: 1))
         }
-        result.addPath(path.trimmedPath(from: max(0, trimFrom), to: offset).strokedPath(.init()))
+        result.addPath(path.trimmedPath(from: max(0, trimFrom), to: offset))
+        result = result.strokedPath(strokeStyle)
         result.addPath(head)
         return result
+    }
+}
+
+struct ArrowHead: Shape {
+    func path(in rect: CGRect) -> Path {
+        return Path { p in
+            p.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+            p.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+            p.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        }.strokedPath(strokeStyle)
     }
 }
 
@@ -74,11 +138,15 @@ struct ContentView: View {
             ZStack {
                 Eight()
                     .stroke(Color.gray)
-                OnPath(shape: rect, pathShape: Eight(), offset: position)
-                    .foregroundColor(.black)
-                .animation(Animation.linear(duration: 5).repeatForever(autoreverses: false))
+                ArrowHead()
+                    .size(width: 30, height: 30)
+                    .offset(x: -15, y: -15)
+                    .modifier(FollowPath(pathShape: Eight(), offset: position))
+                Trail(pathShape: Eight(), offset: position)
             }.onAppear(perform: {
-                self.position = 1
+                withAnimation(Animation.linear(duration: 5).repeatForever(autoreverses:          false)) {
+                    self.position = 1
+                }
             })
             .aspectRatio(16/9, contentMode: .fit)
             .padding(20)
